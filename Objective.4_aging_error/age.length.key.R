b1fbae6572865908f80c006e155f8373704d42a5
John <SwensonJohnD@gmail.com>
2022-08-19 16:00:37 -0400

    Update age-length script

--- Objective.4_aging_error/age.length.key.R
+++ Objective.4_aging_error/age.length.key.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(FSA) #Mainly for von bertalanffy growth function (vbFuns)
+library(RColorBrewer)
 
 rm(list=ls())
 
@@ -109,3 +110,108 @@ ages <- 0:50
 vonBert <- vbFuns(param = "Typical")
 tmp <- growthFunShow("vonBertalanffy","Typical")
 plot(vonBert(t = ages, Linf = 317.65, K = 0.057, t0 = -2.302), pch = 19, type = "b", main = tmp, ylab = "Length", xlab = "Age")
+
+#Copied from above to see sd of length-at-age
+#Look at mean and sd of lengths at different ages
+Age.length.ref %>% group_by(age.at.capture) %>% 
+  summarize(mean.PCL = mean(PCL_cm, na.rm = TRUE),
+            sd.PCL = sd(PCL_cm, na.rm = TRUE),
+            number = n())
+
+#Vector of mean length at age from VonBert curve
+mean_length.at.age <- vonBert(t = ages, Linf = 317.65, K = 0.057, t0 = -2.302)
+
+#Vector of sd length at age, partially from data partially arbitrary
+sd_length.at.age <- c(3, 9, 9, rep(10, times = 48)) #Try 10 so we don't misassign across too many ages
+
+#Combine above into tibble
+length.at.age_df <- tibble(mean = mean_length.at.age, sd = sd_length.at.age)
+
+laf.df = laf.temp <- NULL
+
+for(i in 1:nrow(length.at.age_df)){
+  
+  laf.temp <- tibble(age = rep(i-1, times = 1000)) %>% mutate(length = rnorm(n = 1000, mean = length.at.age_df$mean[i], sd = length.at.age_df$sd[i]))
+
+  laf.df <- bind_rows(laf.df, laf.temp)
+}
+
+n <- 51
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+
+laf.df %>% mutate(age = factor(age)) %>%  
+  gghistogram(x = "length", color = "age", fill = "age", palette = col_vector, add_density = TRUE) +
+  theme(legend.position = "none")
+
+ 
+
+
+
+
+
+
+#### Age Slicing function
+#Use the below function and reference this page: https://stecf.jrc.ec.europa.eu/c/document_library/get_file?uuid=0efd7160-4555-41f5-8234-6e74db26e632&groupId=43805
+
+ageIt=function(len=NULL,n=NULL,Linf=NULL,K=NULL,t0=0.0,timing=0.5,plusGroup=30){
+  ## expected age at length adjust to beginning of year
+  age=pmax(pmin(floor(t0-log(1-pmin(len/Linf,.9999999))/K+timing),plusGroup),0)
+  
+  ## calculate frequencies
+  res=aggregate(n, list(age=age), sum)
+  
+  return(res)}                  
+
+
+
+
+
+#------------------------From Liz--------------------------------#
+#Multiplicative lets it change more with age
+Linf <- 100
+K <- 0.25
+t0 <- -0.05
+
+ages <- seq(1,19)
+len <- seq(1,100)
+# ---
+vonbert <-function(x,Linf,K,t0=0){
+  
+  
+  y=Linf*(1.0-exp(-K*(x-t0)))
+  return(y)
+  
+}
+# ---
+
+mean.len.age<-vonbert(ages, Linf, K, t0)   #vector of length at age
+sd.len.add <- 18
+# additive
+age.len.mat.add <- sapply(mean.len.age, function(x) qnorm(p=seq(0.05, 0.95, by=0.05), mean=x, sd=sd.len.add))
+
+# multiplicative
+sd.len.mult <- 0.2
+#Each cell is length; column is age; each row is percentile, going by 5%
+age.len.mat.mult <- exp(sapply(log(mean.len.age), function(x) qnorm(p=seq(0.05, 0.95, by=0.05), mean=x, sd=sd.len.mult)) )
+
+#I can loop through the dataframe length.at.age_df and make the matrix of quantiles from Liz
+#I'd have the true age for each individual; I'm looking to get a matrix where the diagonal is the highest value and the row sums to 1
+#I'd take the true age, then sample from a vector of misassigned ages and pass it the probabilities; it will be flatter with higher ages
+#The exact matrix Liz sent is probably early on in the process
+
+int_f <- function(x, mu1, mu2, sd1, sd2) {
+  f1 <- dnorm(x, mean=mu1, sd=sd1)
+  f2 <- dnorm(x, mean=mu2, sd=sd2)
+  pmin(f1, f2)
+}
+
+m.a1 <- 30
+sd.a1 <- 5
+m.a2 <- 45
+sd.a2 <- 5
+m.a3 <- 50
+sd.a3 <- 5
+
+integrate(int_f, -Inf, Inf, mu1=m.a1, mu2=m.a2, sd1=sd.a1, sd2=sd.a3)
